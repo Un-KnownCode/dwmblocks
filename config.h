3184f03db58be5d210c463f32eb3c9a9ba95d9b9
Un-KnownCode <1183063391@qq.com>
2020-12-18 19:21:21 +0800

    dwmblocks

---
+++ config.h
@@ -0,0 +1,18 @@
+static const Block blocks[] = {
+    {""   , "/home/unknowncode/my.scripts/dwm.getNetTraf.sh"   , 1     , 16} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getMusic.sh"     , 0     , 11} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getMail.sh"      , 0     , 12} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getCpu.sh"       , 10    , 13} ,
+    //{""   , "rainbarf.sh"         , 1     , 18} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getMemory.sh"    , 10    , 14} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getDisk.sh"      , 10    , 15} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getVolume.sh"    , 0     , 10} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getBattery.sh"   , 5     , 3}  ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getWeather.sh"   , 18000 , 5}  ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getClock.sh"     , 60    , 1}  ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getMoonPhase.sh" , 18000 , 17} ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getInternet.sh"  , 5     , 4}  ,
+    {""   , "/home/unknowncode/my.scripts/dwm.getPackages.sh"  , 0     , 8}  ,
+};
+
+static char *delim = ",";
